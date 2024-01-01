1ddd9c47bafe42187492c7b054a8b7f33222024e
0113bernoyoun <gudwls1530@gmail.com>
2024-01-01 17:05:53 +0900

    chore : add grpc gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+import com.google.protobuf.gradle.*
 plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    id ("com.google.protobuf") version "0.9.4"
     kotlin("plugin.noarg") version "1.5.30"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
 }
+
+apply(plugin = "com.google.protobuf")
 
 group = "com.cooffee"
 version = "0.0.1-SNAPSHOT"
@@ -19,25 +22,65 @@ noArg {
     annotation("jakarta.persistence.Entity") // Java EE면 javax.persistence.Entity
 }
 
+val grpcVersion = "1.59.1"
+val protobufVersion = "3.25.1"
+val protocVersion = protobufVersion
+
 repositories {
     mavenCentral()
 }
-
+sourceSets {
+    main {
+        java {
+            srcDir("cooffee-rpc")
+        }
+    }
+}
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation ("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation ("io.grpc:grpc-stub:${grpcVersion}")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:${protocVersion}" }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            task -> task.builtins {
+                all().forEach {
+                    it.plugins {
+                        id("grpc")
+                    }
+                }
+        }
+//        ofSourceSets("main".forEach {
+//            all().forEach {
+//                it.plugins {
+//                    id("grpc")
+//                }
+//            }
+        }
+        setGeneratedFilesBaseDir("$projectDir/src/generatedProto")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
